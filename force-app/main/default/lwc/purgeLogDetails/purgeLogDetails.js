270be4d3b7d38036d568cc74aa23f4f56e36440b
ranjans3091 <37045532+ranjans3091@users.noreply.github.com>
2023-08-01 20:27:35 +0100

    Delete force-app/main/default/lwc/purgeLogDetails directory

--- force-app/main/default/lwc/purgeLogDetails/purgeLogDetails.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-import SVG_LOGO from '@salesforce/resourceUrl/logTraceIcon';
-
-
-export default class PurgeLogDetails extends LightningElement {
-    svgURL = `${SVG_LOGO}#logTraceLogoId`
-}
